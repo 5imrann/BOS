16c74c492ce8df472f4ce1d4c193c8743cc79d00
5imrann <ssiimmrraann72@gmail.com>
2026-06-03 22:28:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/home'));
+app.use('/services', require('./routes/services'));
+app.use('/about', require('./routes/about'));
+app.use('/contact', require('./routes/contact'));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
